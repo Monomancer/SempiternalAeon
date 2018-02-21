1444472fb078a2dfd1130454a4e8df8d78062fb2
Andrew Oslica <asoslica@gmail.com>
2018-02-21 17:38:22 -0500

    Basic AI fixed player animation
    title

---
+++ SempA2/Assets/Standard Assets/2D/Scripts/FollowCharacter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCharacter : MonoBehaviour {
+
+    public Transform target;
+    public float x_Offset;
+    public float y_Offset;
+
+
+    public Vector3 cameraPosition;
+
+    // Use this for initialization
+    void Start () {
+        cameraPosition = target.position;
+        cameraPosition.x += x_Offset;
+        cameraPosition.y += y_Offset;
+        transform.position = cameraPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cameraPosition = target.position;
+        cameraPosition.x += x_Offset;
+        cameraPosition.y += y_Offset;
+        transform.position = cameraPosition;
+    }
+}
