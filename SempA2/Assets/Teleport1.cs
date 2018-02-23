99e9884a9d26b9674205f1d4c79043e66624f88e
Andrew Oslica <asoslica@gmail.com>
2018-02-23 14:57:36 -0500

    Teleporting dude

---
+++ SempA2/Assets/Teleport1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport1 : MonoBehaviour
+{
+	void OnCollisionEnter2D (Collision2D col)
+	{
+
+		if (col.gameObject.tag == "Player") {
+			col.gameObject.transform.position = new Vector3 (12, 0, 0);
+		}
+	}
+}
