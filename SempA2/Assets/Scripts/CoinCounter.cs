9720d7bd9dc5246d28f402f6f09e0e7c616fe37e
Andrew Oslica <asoslica@gmail.com>
2018-03-28 18:06:24 -0400

    merged monster + coins

--- SempA2/Assets/Scripts/CoinCounter.cs
+++ SempA2/Assets/Scripts/CoinCounter.cs
@@ -4,24 +4,27 @@ using System;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CoinCounter : MonoBehaviour {
+public class CoinCounter : MonoBehaviour
+{
 
-    //to reference text in canvas coin object
-    private static Text coinText;
+	//to reference text in canvas coin object
+	private static Text coinText;
 
-    //count for number of coins
-    private static int coinCount = 0;
+	//count for number of coins
+	private static int coinCount = 0;
 
 	// Use this for initialization
-	void Start () {
-        coinText = GetComponent<Text>(); //get text object
-        coinText.text = "Coins: " + "0"; //set display to 0 coins
+	void Start ()
+	{
+		coinText = GetComponent<Text> (); //get text object
+		coinText.text = "Coins: " + coinCount; //set display to 0 coins
 
-    }
+	}
 
-    public static void incrementCoinCount() {
-            coinCount++; //increment coin count
-            coinText.text = "Coins: " + coinCount.ToString(); //set display to new coin count
+	public static void incrementCoinCount ()
+	{
+		coinCount++; //increment coin count
+		coinText.text = "Coins: " + coinCount.ToString (); //set display to new coin count
         
-    }
+	}
 }
