985c066abde4dd428d93441de3c890c95b495e88
Justin <36085007+Jooostan@users.noreply.github.com>
2018-03-28 02:35:07 -0400

    added coin count to canvas

---
+++ SempA2/Assets/Scripts/coinBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class coinBehavior : MonoBehaviour {
+
+    public Transform player;
+    private Transform coin;
+
+    // Use this for initialization
+    void Start () {
+        coin = GetComponent<Transform>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Math.Abs(player.position.x - coin.position.x) < .1 && Math.Abs(player.position.x - coin.position.x) < .5)
+        {
+            CoinCounter.incrementCoinCount();
+            Destroy(gameObject);
+        }
+		
+	}
+}
