b0c53914043344522a892e7c48b18ddad0c38060
Benjamin Kauppi <bikauppi@buffalo.edu>
2018-04-10 15:46:04 -0400

    Added Assets from Asset store added new scene for town (no ground yet) added script for scene transition onClick attached to signpost

---
+++ SempA2/Assets/CreateNewScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreateNewScene : MonoBehaviour {
+
+    // Use this for initialization
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene("Grassy Scene", LoadSceneMode.Single);
+        
+        
+    }
+
+}
+	
+
+
