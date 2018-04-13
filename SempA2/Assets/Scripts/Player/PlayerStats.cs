b31a89c90ce2080abda3c6ca6cfe2e19a0e7d893
Benjamin Kauppi <bikauppi@buffalo.edu>
2018-04-13 14:54:22 -0400

    quest tracking seems to be working along with a few minor fixes to grassy scene to prevent player from leaving the area (sorry for the really janky code I was pressed for time and just trying whatever worked)

--- SempA2/Assets/Scripts/Player/PlayerStats.cs
+++ SempA2/Assets/Scripts/Player/PlayerStats.cs
@@ -7,7 +7,9 @@ public class PlayerStats : MonoBehaviour
 	public int currentLevel;
 	public int currentExp;
 	public int expModifier;
-    public static int batsKilled;
+    private static int questBatsKilled;
+    private static int batsKilled;
+    private static bool onQuest;
 
 	// Use this for initialization
 	void Start ()
@@ -28,10 +30,34 @@ public class PlayerStats : MonoBehaviour
         batsKilled++;
         Debug.Log("bats killed incremented");
         Debug.Log("bats killed = " + batsKilled);
+        if (onQuest)
+        {
+            questBatsKilled++;
+            Debug.Log("Quest bats killed = " + questBatsKilled);
+        }
     }
 
-    public int batsKilledgetter()
+    public int BatsKilledgetter()
     {
         return batsKilled;
+    }
+    public int QuestBatsKilledgetter()
+    {
+        return questBatsKilled;
+    }
+
+
+    public bool OnQuestgetter()
+    {
+        return onQuest;
+    }
+    public void OnQuestReset()
+    {
+        onQuest = false;
+        questBatsKilled = 0;
+    }
+    public void OnQuestSetTrue()
+    {
+        onQuest = true;
     }
 }
--- SempA2/Assets/Scripts/Quests/QuestHandler.cs
+++ SempA2/Assets/Scripts/Quests/QuestHandler.cs
@@ -9,11 +9,15 @@ public class QuestHandler : MonoBehaviour
 {
 
     public string npcName;
-    public string[] dialogueArray;
+    public string[] questStartDialogueArray;
+    public string[] questInProgressDialogueArray;
+    public string[] questCompletedDialogueArray;
+    public string[] questErrorDialogueArray;
     private DialogueManager dMan;
     public bool hasMonsterQuest = false;
     public String monsterName;
-    public int amount, experience;
+    public int killsRequired;
+    public int experience;
     private bool inDialogueRange = false;
 
     // Use this for initialization
@@ -26,24 +30,98 @@ public class QuestHandler : MonoBehaviour
 
     private void OnMouseDown()
     {
-       var x = GameObject.FindGameObjectWithTag("UICanvas").GetComponent<PlayerStats>().batsKilledgetter();
+       var batsKilled = GameObject.FindGameObjectWithTag("UICanvas").GetComponent<PlayerStats>().BatsKilledgetter();
+       var questBatsKilled = GameObject.FindGameObjectWithTag("UICanvas").GetComponent<PlayerStats>().QuestBatsKilledgetter();
+        hasMonsterQuest = GameObject.FindGameObjectWithTag("UICanvas").GetComponent<PlayerStats>().OnQuestgetter();
         Debug.Log("trigger hit");
-        Debug.Log("batskilled = " + x);
+        Debug.Log("batskilled = " + batsKilled);
+        Debug.Log("questbatsKilled = " + questBatsKilled);
+
+
         if (!dMan.dialogueActive)
         {
             dMan.ActivateDialogueBox();
         }
-        if (dialogueArray.Length > 0)
+
+        // QUEST STARTING SECTION
+
+        if(hasMonsterQuest == false)
         {
-            if (npcName.Length > 0)
+            if (questStartDialogueArray.Length > 0)
             {
-                dMan.ShowDialogueBox(dialogueArray, npcName);
-            }
-            else
-            {
-                dMan.ShowDialogueBox(dialogueArray);
+                GameObject.FindGameObjectWithTag("UICanvas").GetComponent<PlayerStats>().OnQuestSetTrue();
+                string batsKilledString = killsRequired + " bats";
+                questStartDialogueArray[4] += batsKilledString;
+                //start tracking kills required by taking kills required and adding it to the kills we completed before being assigned the quest
+                if (npcName.Length > 0)
+                {
+                    dMan.ShowDialogueBox(questStartDialogueArray, npcName);
+                }
+                else
+                {
+                    dMan.ShowDialogueBox(questStartDialogueArray);
+                }
             }
         }
+
+
+        // QUEST IN PROGRESS SECTION
+        else if(hasMonsterQuest == true && questBatsKilled < killsRequired)
+        {
+            if (questInProgressDialogueArray.Length > 0)
+            {
+                if (npcName.Length > 0)
+                {
+                    dMan.ShowDialogueBox(questInProgressDialogueArray, npcName);
+                }
+                else
+                {
+                    dMan.ShowDialogueBox(questInProgressDialogueArray);
+                }
+            }
+        }
+
+        //QUEST COMPLETED SECTION
+        else if (hasMonsterQuest == true && questBatsKilled >= killsRequired)
+        {
+            //reset the quest, give rewards, and whatever
+            GameObject.FindGameObjectWithTag("UICanvas").GetComponent<PlayerStats>().OnQuestReset();
+            killsRequired += 2;
+
+            if (questCompletedDialogueArray.Length > 0)
+            {
+                if (npcName.Length > 0)
+                {
+                    dMan.ShowDialogueBox(questCompletedDialogueArray, npcName);
+                }
+                else
+                {
+                    dMan.ShowDialogueBox(questCompletedDialogueArray);
+                }
+            }
+        }
+
+        //this else statement should never be reached. 
+        else
+        {
+            if (questErrorDialogueArray.Length > 0)
+            {
+                if (npcName.Length > 0)
+                {
+                    dMan.ShowDialogueBox(questErrorDialogueArray, npcName);
+                }
+                else
+                {
+                    dMan.ShowDialogueBox(questErrorDialogueArray);
+                }
+            }
+
+        }
+
+
+
+
+
 
     }
 
