dfae2b9cce308eb8c357324957e8d22e8cfceae2
Benjamin Kauppi <bikauppi@buffalo.edu>
2018-04-13 05:16:40 -0400

    Quest scene now has UI canvas with dialogue manager and coincount et c. and it somehow magically stays consistant across scene changes without any added anything so hurray it's freaking magic

---
+++ SempA2/Assets/Scripts/Quests/QuestHandler.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Security.Cryptography.X509Certificates;
+using System;
+using UnityEditor.VersionControl;
+
+public class QuestHandler : MonoBehaviour
+{
+
+    public string npcName;
+    public string[] dialogueArray;
+    private DialogueManager dMan;
+    public bool hasMonsterQuest = false;
+    public String monsterName;
+    public int amount, experience;
+    private bool inDialogueRange = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        dMan = FindObjectOfType<DialogueManager>();
+        Debug.Log("dMan was found maybe");
+    }
+
+
+    private void OnMouseDown()
+    {
+        Debug.Log("trigger hit");
+        if (!dMan.dialogueActive)
+        {
+            dMan.ActivateDialogueBox();
+        }
+        if (dialogueArray.Length > 0)
+        {
+            if (npcName.Length > 0)
+            {
+                dMan.ShowDialogueBox(dialogueArray, npcName);
+            }
+            else
+            {
+                dMan.ShowDialogueBox(dialogueArray);
+            }
+        }
+
+    }
+
+
+    public MonsterQuest GetQuest()
+    {
+        return null;
+    }
+}
